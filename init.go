a889f94825bd4df96130c335c1f60f7560a2703f
lao liu <laoliu@gmail.com>
2024-06-13 21:27:03 +0800

    update

--- init.go
+++ init.go
@@ -11,30 +11,10 @@ const (
 var redisDB *redis.Client
 var redisDB_H *redis.Client
 
-func Init(db *redis.Client, db2 *redis.Client) {
+func InitDB(db *redis.Client) {
 	redisDB = db
-	redisDB_H = db2
-	// InitHtxUtil()
 }
 
-// func InitHtxUtil() {
-// 	// 初始化火币交易所配置
-// 	apiConfigs, err := (&ExchangeConfig{
-// 		ExchangeName: EXCHANGE_HTX,
-// 	}).GetString("api")
-// 	if err != nil {
-// 		panic(fmt.Errorf("%s 初始化失败：%s", "htx", err))
-// 	}
-// 	accConfigs, err := (&ExchangeConfig{
-// 		ExchangeName: EXCHANGE_HTX,
-// 	}).Get("account")
-// 	if err != nil {
-// 		panic(fmt.Errorf("%s 初始化失败：%s", "htx", err))
-// 	}
-// 	apiConfig := htx.ApiConfigModel{
-// 		AccountId: util.ParseInt(accConfigs["spot_account_id"], 0),
-// 		Uid:       util.ParseInt(accConfigs["uid"], 0),
-// 	}
-// 	json.Unmarshal([]byte(apiConfigs), &apiConfig)
-// 	htx.InitConfig(apiConfig)
-// }
+func InitDB_H(db *redis.Client) {
+	redisDB_H = db
+}
