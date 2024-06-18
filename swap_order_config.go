5a8975ba78113c0958d8c5e1a13ddf97d6efe327
lao liu <laoliu@gmail.com>
2024-06-18 17:48:17 +0800

    opt

--- swap_order_config.go
+++ swap_order_config.go
@@ -7,20 +7,20 @@ import (
 )
 
 type SwapOrderItem struct {
-	Exchange    string `json:"exchange"`     // 交易所
-	Symbol      string `json:"symbol"`       // 币对
-	OrderId     string `json:"order_id"`     // 订单编号
-	OrderType   string `json:"order_type"`   // 订单类型 buy-open: 买入开多 buy-close: 买入平空 sell-open: 卖出开空 sell-close: 卖出平多
-	OrderPrice  string `json:"order_price"`  // 下单价格
-	TradePrice  string `json:"trade_price"`  // 成交价格
-	OrderValue  string `json:"order_value"`  // 下单金额
-	TradeValue  string `json:"trade_value"`  // 成交金额
-	OrderVolume string `json:"order_volume"` // 下单数量
-	TradeVolume string `json:"trade_volume"` // 成交数量
-	Status      string `json:"status"`       // 订单状态 1-已下单 2-已成交 8-已撤单
-	CreateAt    int64  `json:"create_at"`    // 下单时间
-	FilledAt    int64  `json:"filled_at"`    // 成交时间
-	CancelAt    int64  `json:"cancel_at"`    // 撤单时间
+	Exchange    string  `json:"exchange"`     // 交易所
+	Symbol      string  `json:"symbol"`       // 币对
+	OrderId     string  `json:"order_id"`     // 订单编号
+	OrderType   string  `json:"order_type"`   // 订单类型 buy-open: 买入开多 buy-close: 买入平空 sell-open: 卖出开空 sell-close: 卖出平多
+	OrderPrice  float64 `json:"order_price"`  // 下单价格
+	TradePrice  float64 `json:"trade_price"`  // 成交价格
+	OrderValue  float64 `json:"order_value"`  // 下单金额
+	TradeValue  float64 `json:"trade_value"`  // 成交金额
+	OrderVolume int64   `json:"order_volume"` // 下单数量 张
+	TradeVolume int64   `json:"trade_volume"` // 成交数量 张
+	Status      int64   `json:"status"`       // 订单状态 1-已下单 2-已成交 8-已撤单
+	CreateAt    int64   `json:"create_at"`    // 下单时间
+	FilledAt    int64   `json:"filled_at"`    // 成交时间
+	CancelAt    int64   `json:"cancel_at"`    // 撤单时间
 
 }
 
